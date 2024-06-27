9ef3e14ed730ab777a7b1767e73826cdd180359d
Manas <140826943+isthismanas@users.noreply.github.com>
2024-06-27 18:18:47 +0530

    Added a function to create dataset for char rnn in Utility.py and finsihed the data pipeline

--- Utility.py
+++ Utility.py
@@ -16,3 +16,13 @@ class LNSimpleRNNCell(tf.keras.layers.Layer):
         outputs, new_states = self.simple_rnn_cell(inputs, states)
         norm_outputs = self.activation(self.layer_norm(outputs))
         return norm_outputs , [norm_outputs]
+    
+
+def to_dataset(sequence, length, shuffle = False, seed = None, batch_size = 32):
+    ds = tf.data.Dataset.from_tensor_slices(sequence)
+    ds = ds.window(length +1 , shift = 1, drop_remainder= True)
+    ds = ds.flat_map(lambda window_ds : window_ds.batch(length + 1))
+    if shuffle:
+        ds = ds.shuffle(buffer_size=100000, seed = seed)
+    ds =ds.batch(batch_size)
+    return ds.map(lambda window: (window[:, :-1] , window[:,1:])).prefetch(1)
